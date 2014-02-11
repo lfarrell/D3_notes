a4bcf315828779db5ef7c03ea5993b1eadcd5b24
Larry Farrell <farrelldlfarrell@gmail.com>
2014-02-10 21:45:42 -0500

    Added basic line path generation

---
+++ basic-line.js
@@ -0,0 +1,34 @@
+/**
+ * Set SVG canvas and margins
+ *
+ */
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+var data = [
+    {x: 2, y: 2},
+    {x: 32, y: 15},
+    {x: 43, y: 99}
+]
+
+// Create grouping for path line
+var group = svg.append("g")
+    .attr("transform", "translate(100, 100)")
+
+var line = d3.svg.line()
+    .x(function(d) { return d.x; })
+    .y(function(d) { return y.x; });
+
+group.selectAll("path")
+    .data([data])
+    .enter()
+    .append("path")
+    .attr("d", line) // create draw attribute by passing values to the line var
+    .attr("fill", "none")
+    .attr("stroke", "black")
+    .attr("stroke-width", 2);
