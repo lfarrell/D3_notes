a235be3adc1250e8704f6726dda7d92e4efe24f2
Larry Farrell <farrelldlfarrell@gmail.com>
2014-02-12 17:49:04 -0500

    Finished force setup

--- basic-force-layout.js
+++ basic-force-layout.js
@@ -47,3 +47,26 @@ var edges = svg.selectAll("line")
         .append("line")
         .style("stroke", "#ccc")
         .style("stroke-width", 1);
+
+// Add the nodes as circles
+var nodes = svg.selectAll("circle")
+    .data(dataset.nodes)
+    .enter()
+    .append("circle")
+    .attr("r", 10)
+    .style("fill", function(d, i) {
+        return colors(i);
+    })
+    .call(force.drag); // makes nodes draggable
+
+force.on("tick", function() { // tick is a measure of time
+    // reset each end of the line based on each end's x and y coordinates
+    edges.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    // update each circle based on each node's x and y coordinates
+    nodes.attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+});
